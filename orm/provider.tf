98ed18e6a684df4cfc360d4079dc20af2393e34c
Brandon Tuttle <btuttle@nvidia.com>
2023-03-28 12:03:04 -0400

    Further remove TagNamespace dependency.
    Signed-off-by: Brandon Tuttle <btuttle@nvidia.com>

--- orm/provider.tf
+++ orm/provider.tf
@@ -23,6 +23,6 @@ provider "oci" {
   #  user_ocid            = var.user_ocid
   #  fingerprint          = var.fingerprint
   #  private_key_path     = var.private_key_path
-  region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
+  # region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
   disable_auto_retries = "true"
 }
