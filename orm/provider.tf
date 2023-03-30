24bf28c7079a21fdea6243cdb219e3a715f7caea
Brandon Tuttle <btuttle@nvidia.com>
2023-03-30 13:35:25 -0400

    Revert private_key_path changes
    Signed-off-by: Brandon Tuttle <btuttle@nvidia.com>

--- orm/provider.tf
+++ orm/provider.tf
@@ -1,5 +1,3 @@
-
-
 terraform {
   required_providers {
     oci = {
@@ -22,6 +20,6 @@ provider "oci" {
   #  user_ocid            = var.user_ocid
   #  fingerprint          = var.fingerprint
   #  private_key_path     = var.private_key_path
-  # region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
+  region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
   disable_auto_retries = "true"
 }
